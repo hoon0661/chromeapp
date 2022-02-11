d02093f8d3f0de05a07c249157b21208a96ab020
Hoon Kim <hoon0661@gmail.com>
2022-02-10 22:17:02 -0500

    added background, and todo list-add&delete

---
+++ js/todo.js
@@ -0,0 +1,29 @@
+const todoForm = document.getElementById("todo-form");
+const toDoInput = document.querySelector("#todo-form input");
+const toDoList = document.getElementById("todo-list");
+
+function paintToDo(newTodo) {
+  const li = document.createElement("li");
+  const span = document.createElement("span");
+  span.innerText = newTodo;
+  const button = document.createElement("button");
+  button.innerText = "❌";
+  button.addEventListener("click", deleteTodo);
+  li.appendChild(span);
+  li.appendChild(button);
+  toDoList.appendChild(li);
+}
+
+function deleteTodo(event) {
+  const li = event.target.parentElement;
+  li.remove();
+}
+
+function handleToDoSubmit(event) {
+  event.preventDefault();
+  const newTodo = toDoInput.value;
+  toDoInput.value = "";
+  paintToDo(newTodo);
+}
+
+todoForm.addEventListener("submit", handleToDoSubmit);
