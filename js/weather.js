a464f675dc9bd544f5e2bf6ed3ca44c8dcc88324
Hoon Kim <hoon0661@gmail.com>
2022-02-11 23:34:56 -0500

    added weather

---
+++ js/weather.js
@@ -0,0 +1,20 @@
+const API_KEY = "c5bf70503882c71db48436245d185d2c";
+
+function onGeoOk(position) {
+  const lat = position.coords.latitude;
+  const lon = position.coords.longitude;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=imperial`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const weather = document.querySelector("#weather span:first-child");
+      const city = document.querySelector("#weather span:last-child");
+      weather.innerText = data.weather[0].main;
+      city.innerText = data.name;
+    });
+}
+function onGeoError() {
+  alert("Can't find you. No weather for you.");
+}
+
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
